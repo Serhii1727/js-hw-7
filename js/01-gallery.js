70c3c40af0b43b4dfafed9524e7512ff0eb300c3
Serhii1727 <kravchenko087@ukr.net>
2023-10-13 11:46:13 -0400

    add Task 1 (Image gallery)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const makeMarkupGallery = (galleryItems) => {
+    const markup = galleryItems.map(({preview, description, original}) => `<li class='gallery__item'><img src=${preview}  alt=${description}  data-original=${original} class='gallery__image'></li>
+    `)
+    galleryRef.insertAdjacentHTML('beforeend', markup.join(''))
+}
+
+makeMarkupGallery(galleryItems);
+
+galleryRef.addEventListener('click', (event) => {
+    console.dir(event.target)
+    if (event.current.nodeName !== 'IMG') {
+        return
+    }
+    const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.original} width="800" height="600">
+`)
+    instance.show()
+})
